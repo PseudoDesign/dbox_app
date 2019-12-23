9faa013eb913a3c23ace2271bfa25839e107d8e8
Adam <PseudoDesign@users.noreply.github.com>
2019-12-22 23:35:03 -0700

    added tests for ioerror and yamlerror

--- dbox_app/lock_authorization/lock.py
+++ dbox_app/lock_authorization/lock.py
@@ -1,27 +1,41 @@
+import yaml
+
 class Lock:
     """
     Manages a secure key lock, where the lock is stored on the filesystem.
     """
 
-    def __init__(self, lock_path):
+    def __init__(self, lock_path: str):
         """
         :param lock_path: filesystem location of the lock file.
         """
         self.__lock_path = lock_path
 
     @property
-    def is_locked(self):
+    def is_locked(self) -> bool:
+        """
+        :return: True if the device is locked, else false
+        """
         return self._is_file_valid
 
     @property
-    def is_unlocked(self):
+    def is_unlocked(self) -> bool:
+        """
+        :return: True if the device is unlocked, else false
+        """
         return not self.is_locked
 
     @property
-    def _is_file_valid(self):
+    def _is_file_valid(self) -> bool:
         """
         Returns if the lock file at self.__lock_path exists and is valid, else false
         :return:
         """
-        return False
+        try:
+            lock_info = self._load_lock_file()
+        except (IOError, yaml.YAMLError):
+            return False
+        return lock_info
 
+    def _load_lock_file(self) -> {}:
+        return {}
--- tests/test_lock_authorization.py
+++ tests/test_lock_authorization.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from unittest.mock import patch, PropertyMock
 from dbox_app.lock_authorization import Lock
+import yaml
 
 
 class TestLock(TestCase):
@@ -23,4 +24,16 @@ class TestLock(TestCase):
     def test_lock_is_not_unlocked_when_key_file_is_not_valid(self, mock_is_file_valid):
         mock_is_file_valid.return_value = True
         self.assertFalse(Lock("test_path").is_unlocked)
+
+    @patch.object(Lock, "_load_lock_file")
+    def test_lock_is_unlocked_when_key_file_cannot_be_opened(self, mock_load_lock_file):
+        mock_load_lock_file.side_effect = IOError()
+        self.assertTrue(Lock("test_path").is_unlocked)
+
+    @patch.object(Lock, "_load_lock_file")
+    def test_lock_is_unlocked_when_key_file_yaml_cannot_be_parsed(self, mock_load_lock_file):
+        mock_load_lock_file.side_effect = yaml.YAMLError()
+        self.assertTrue(Lock("test_path").is_unlocked)
+
+
 
