274d44b484b2d398e0ef059f2afcffeefb73607c
Adam <PseudoDesign@users.noreply.github.com>
2020-01-15 23:45:51 -0700

    added requirements

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],  # Optional
+    install_requires=['PyYAML', 'bcrypt', 'colorzero', 'gpiozero', 'transitions'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
