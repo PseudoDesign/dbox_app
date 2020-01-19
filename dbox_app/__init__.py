2ed71c9f338ec67e8c32bed7edee3832e6dd8ce9
Adam Schafer <PseudoDesign@users.noreply.github.com>
2020-01-18 21:19:44 -0700

    updated application launcher

--- dbox_app/__init__.py
+++ dbox_app/__init__.py
@@ -1,12 +1,34 @@
 from .state_machine import StateMachine
 from .lock_authorization import SecureLock
 from .phy import Button, Latch, RgbLed
+import argparse
+import yaml
 
 
 def main():
-    button = Button(3)
-    latch = Latch(2)
-    led = RgbLed(17, 4, 18)
-    lock = SecureLock("/nvm/test_lock.yaml")
+    parser = argparse.ArgumentParser(description="Launcher for the dbox app")
+    parser.add_argument('config_file', type=str, default="/etc/dbox/conf", help="Location of the donfi")
+
+    args = parser.parse_args()
+
+    # Open the yaml file and ensure parameters are in the file
+    try:
+        with open(args.config_file, 'r') as fpt:
+            config_data = yaml.safe_load(fpt)
+            button_pin = int(config_data["button"])
+            latch_pin = int(config_data["latch"])
+            red_led_pin = int(config_data["led_r"])
+            green_led_pin = int(config_data["led_g"])
+            blue_led_pin = int(config_data["led_b"])
+            secure_lock_file = config_data["lock_file"]
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Unable to open config file {args.config_file}.  See README for details")
+    except (KeyError, yaml.YAMLError):
+        raise ValueError(f"Invalid config file {args.config_file}.  See README for details")
+
+    button = Button(button_pin)
+    latch = Latch(latch_pin)
+    led = RgbLed(red_led_pin, green_led_pin, blue_led_pin)
+    lock = SecureLock(secure_lock_file)
 
     machine = StateMachine(button, led, lock, None, latch)
